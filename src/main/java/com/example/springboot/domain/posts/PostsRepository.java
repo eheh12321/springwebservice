c8a4e361bd0b41278c5062fb195336051070c12b
DunDun <eheh12321@gmail.com>
2022-08-07 18:45:53 +0900

    챕터 4 - mustache 활용

--- src/main/java/com/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/example/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // Entity 클래스와 "기본" Entity Repository는 항상 함께 위치하도록 한다
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
